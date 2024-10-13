c4e20bbbcedcde72cb1473738adab6b64d740109
VadimProcik <b00132224@mytudublin.ie>
2024-10-13 19:16:08 +0100

    text

--- ConstructionPal/App.js
+++ ConstructionPal/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Homeology Companian App Time to make a dddPDF</Text>
+      <Text>Homeology Companian App</Text>
       <StatusBar style="auto" />
     </View>
   );
